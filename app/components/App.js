cd63f0a29e0b029d72c1bab692f8052c5399ed9f
scott <scott@mbp123.local>
2017-03-26 15:04:35 +0800

    this.props.children

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-class App extends React.Component{
-	render(){
-		return (
-			<div>
-			<h1>test</h1>
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+			<h1>App</h1>
+			{this.props.children}
 			</div>
-			)
-	}
+        )
+    }
 }
 
 export default App;
---
+++ app/components/Stats.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Stats extends React.Component {
+    render() {
+        return (
+            <div>
+          <h1>Stats</h1>
+        </div>
+        )
+    }
+}
+
+export default Stats;
